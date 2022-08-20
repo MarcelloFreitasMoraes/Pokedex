0f3e89b4019a765d01ace48a221d4f0a95c0bdf7
Marcelo de Freitas Moraes <mamoressp@gmail.com>
2022-08-20 13:37:00 -0300

    ajustes

--- container/index.jsx
+++ container/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import * as S from "../styles/styles.container";
 
-export const Container = ({ children }) => {
-  return <S.Main>{children}</S.Main>;
-};
+export const Container = ({children}) => {
+
+    return (
+        <S.Main>
+            {children}
+        </S.Main>
+    )
+}
